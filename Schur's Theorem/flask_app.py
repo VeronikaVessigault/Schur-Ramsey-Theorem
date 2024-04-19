35bfdb2d9862d2d3b44015b2b5e4951b1f2ba156
VeronikaVessigault <108096639+VeronikaVessigault@users.noreply.github.com>
2024-04-19 16:13:53 -0400

    Add files via upload

---
+++ Schur's Theorem/flask_app.py
@@ -0,0 +1,97 @@
+from math import remainder
+from flask import Flask, render_template, request, redirect, url_for
+import sys
+import os
+import csv
+import schur
+ 
+app = Flask(__name__, static_url_path = "/upload", static_folder = "upload")
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/show-schur', methods=["GET"])
+def visualization():
+    return render_template('show_schur.html')
+
+@app.route('/run-schur', methods =["GET", "POST"])
+def bulk_random_walk():
+    dim = -1
+    steps = -1
+    runs = -1
+    if request.method == 'POST':
+        dim = request.form['dim']
+        steps = request.form['steps']
+        runs = request.form['number of walks']
+
+        #walks.set_valid_keys_by_steps()
+        #walks.set_valid_keys_by_dimension()
+
+        allowed_dim = [1,2,3,4,5,6,7,8]
+        if int(dim) in allowed_dim and int(steps)>199 and int(steps)<15001 and int(runs) > 0 and int(runs) < 100001:
+            if int(steps) < 7501 and int(steps) % 100 == 0:
+                ending_vecs = []
+                how_many_rec = []
+                #print(ending_vecs)
+                percent_rec = str(round(how_many_rec/int(runs)*100,4)) + "%"
+                vectors = []
+                return render_template('random_walk_bulk_w_stats.html', vectors = vectors, how_many_rec = how_many_rec, percent_rec = percent_rec, dim = dim, steps = steps, runs = runs)
+            elif int(steps) % 500 == 0:
+                ending_vecs = []
+                how_many_rec = []
+                percent_rec = str(round(how_many_rec/int(runs)*100,4)) + "%"
+                vectors = []
+                return render_template('show-schur.html', vectors = vectors, how_many_rec = how_many_rec, percent_rec = percent_rec, dim = dim, steps = steps, runs = runs)
+        else:    
+            dim_right = False
+            steps_right = False
+            runs_right = False
+            if int(dim) in allowed_dim:
+                dim_right = True
+            if (int(steps) > 199 and int(steps) < 7501 and int(steps) % 100 == 0) or (int(steps) > 7499 and int(steps) < 15001 and int(steps) % 500 == 0):
+                steps_right = True
+            if int(runs) > 0 and int(runs) < 25001:
+                runs_right = True
+            if not (dim_right or steps_right or runs_right):
+                return render_template('random_walk_bulk.html', error_message = "error_message_dim_steps_runs.html", dim = dim, steps = steps, runs = runs)
+            elif not (dim_right or steps_right):
+                return render_template('random_walk_bulk.html', error_message = "error_message_dim_steps.html", dim = dim, steps = steps)
+            elif not (dim_right or runs_right):
+                return render_template('random_walk_bulk.html', error_message = "error_message_dim_runs.html", dim = dim, runs = runs)
+            elif not (steps_right or runs_right):
+                return render_template('random_walk_bulk.html', error_message = "error_message_steps_runs.html", steps = steps, runs = runs)
+            elif not dim_right:
+                return render_template('random_walk_bulk.html', error_message = "error_message_dim.html", dim = dim)
+            elif not steps_right:
+                return render_template('random_walk_bulk.html', error_message = "error_message_steps.html", steps = steps)
+            elif not runs_right:
+                return render_template('random_walk_bulk.html', error_message = "error_message_runs.html", runs = runs)
+    return render_template('run_schur.html', error_message = "empty.html")
+
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('data.html',form_data = form_data)
+ 
+ 
+#app.run(host='localhost', port=5000)
---
+++ Schur's Theorem/schur.py
@@ -0,0 +1,125 @@
+from random import SystemRandom
+from math import factorial, ceil
+from math import e as e
+
+cryto_gen_num = SystemRandom()
+
+import os
+import csv 
+
+
+def get_list_csv(filename):
+    file_list = ["keys"]
+    if os.path.isfile("{}.csv".format(filename)) and filename in file_list:
+        with open('{}.csv'.format(filename)) as csv_file:
+            reader = csv.reader(csv_file)
+            mylist = list(reader)
+            list_tuples = []
+            for i in mylist[0]:
+                list_tuples.append(eval(i))
+            return list_tuples
+    return list()
+
+
+def set_list_csv(filename, mylist):
+    file_list = ["keys"]
+    if os.path.isfile("{}.csv".format(filename)) and filename in file_list:
+        with open('{}.csv'.format(filename), 'w') as csv_file:  
+            writer = csv.writer(csv_file)
+            writer.writerow(mylist)
+
+
+def set_keys():
+    valid_keys = [] # colorings
+
+    my_list = [(2,j) for j in range(2, ceil(factorial(2)*e))]
+    my_list.append((2, ceil(factorial(2)*e)))
+    my_list.sort()
+    valid_keys.extend(my_list) # 2 coloring, j vertices
+
+    my_list = [(3,j) for j in range(2, ceil(factorial(3)*e))]
+    my_list.append((3, ceil(factorial(3)*e)))
+    my_list.sort()
+    valid_keys.extend(my_list) # 3 coloring
+
+    my_list = [(4,j) for j in range(2, ceil(factorial(4)*e),5)]
+    my_list.append((4, ceil(factorial(4)*e)))
+    my_list.sort()
+    valid_keys.extend(my_list) # 4 coloring
+
+    my_list = [(5,j) for j in range(10, ceil(factorial(5)*e),10)]
+    my_list.append((5, ceil(factorial(5)*e)))
+    my_list.sort()
+    valid_keys.extend(my_list) # 5 coloring
+
+    my_list = [(6,j) for j in range(100, ceil(factorial(6)*e),100)]
+    my_list.extend([(6, ceil(factorial(6)*e)), (6, 10), (6, 50)])
+    my_list.sort()
+    valid_keys.extend(my_list) # 6 coloring
+
+    my_list = [(7,j) for j in range(700, ceil(factorial(7)*e),700)]
+    my_list.extend([(7, ceil(factorial(7)*e)), (7, 10), (7, 50), (7, 100), (7, 250), (7, 500)])
+    my_list.sort()
+    valid_keys.extend(my_list) # 7 coloring
+
+    my_list = [(8,j) for j in range(5000,ceil(factorial(8)*e),5000)]
+    my_list.extend([(8, ceil(factorial(8)*e)), (8, 10), (8, 50), (8, 100), (8, 250), (8, 500), (8, 1000), (8, 2500)])
+    my_list.sort()
+    valid_keys.extend(my_list) # 8 coloring
+
+    my_list = [(9,j) for j in range(15000,ceil(factorial(9)*e),15000)]
+    my_list.extend([(9, ceil(factorial(9)*e)), (9, 10), (9, 50), (9, 100), (9, 250), (9, 500), (9, 1000), (9, 2500), (9, 5000), (9, 10000)])
+    my_list.sort()
+    valid_keys.extend(my_list) # 9 coloring
+
+    my_list = [(10,j) for j in range(50000,ceil(factorial(10)*e),50000)]
+    my_list.extend([(10, ceil(factorial(10)*e)), (10, 10), (10, 50), (10, 100), (10, 250), (10, 500), (10, 1000), (10, 2500), (10, 5000), (10,10000), (10, 25000)])
+    my_list.sort()
+    valid_keys.extend(my_list) # 10 coloring
+
+    set_list_csv("keys", valid_keys)
+
+
+def run_coloring(n = -1, r = -1, check = False, doubles = False):
+    colors = ["red", "blue", "purple", "yellow", "lime", "fuchsia", "orange", "maroon", "cyan", "green"]
+    r = 5
+    n = ceil(factorial(r)*e)
+    parts = {i : list() for i in colors[:r]}
+    for i in range(1, n+1):
+        rand_num = cryto_gen_num.randrange(r)
+        parts[colors[rand_num]].append(i)
+    
+    smallest = False
+    if check:
+        check, smallest = check_schur(parts, n, r, doubles)
+    return (parts, check, smallest)
+
+def check_schur(parts = {}, n = -1, r = -1, doubles = False, smallest = False):
+    colors = ["red", "blue", "purple", "yellow", "lime", "fuchsia", "orange", "maroon", "cyan", "green"]
+    good_schurs = []
+    r = 5
+    n = ceil(factorial(r)*e)
+    
+    smallest_num = False
+        
+    for i in range(0, r):
+        my_list = parts[colors[i]]
+        list_len = len(my_list)
+        for j in range(0, list_len):
+            if doubles:
+                for k in range(j, list_len):
+                    if my_list[j] + my_list[k] in my_list:
+                        good_schurs.append((colors[i], my_list[j], my_list[k], my_list[j] + my_list[k]))
+            else:
+                for k in range(j+1, list_len):
+                    if my_list[j] + my_list[k] in my_list:
+                        if smallest:
+                            if not smallest_num:
+                                smallest_num = my_list[j] + my_list[k]
+                            elif smallest_num > my_list[j] + my_list[k]:
+                                smallest_num = my_list[j] + my_list[k]
+                        good_schurs.append((colors[i], my_list[j], my_list[k], my_list[j] + my_list[k]))
+    return good_schurs, smallest_num
+
+
+ 
